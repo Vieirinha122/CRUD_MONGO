4a9b8774c4867291e6261ee62f82c17ec7c30056
Arthur Vieira <Arthurv720@gmail.com>
2025-04-07 10:52:14 -0300

    Correção nome de arquivo App.ts para app.ts

--- src/api/App.ts
+++ src/api/App.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import contatoRoutes from './../routes/contatoRoutes';
+import contatoRoutes from '../routes/contatoRoutes';
 
 dotenv.config();
 
